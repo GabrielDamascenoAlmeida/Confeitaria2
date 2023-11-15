edabd66ae5e01bb8cd7cd17c3f3676cc68393934
Rmm20232930010 <Rmm20232930010@LAB1-03>
2023-11-14 22:24:58 -0300

    Adicionar arquivos de projeto.

---
+++ Confeitaria2/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace Confeitaria2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtVal = new System.Windows.Forms.TextBox();
+            this.btnExc = new System.Windows.Forms.Button();
+            this.btnConf = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvList = new System.Windows.Forms.DataGridView();
+            this.txtQuant = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtVal
+            // 
+            this.txtVal.Location = new System.Drawing.Point(117, 156);
+            this.txtVal.Name = "txtVal";
+            this.txtVal.Size = new System.Drawing.Size(57, 20);
+            this.txtVal.TabIndex = 25;
+            // 
+            // btnExc
+            // 
+            this.btnExc.Location = new System.Drawing.Point(555, 263);
+            this.btnExc.Name = "btnExc";
+            this.btnExc.Size = new System.Drawing.Size(112, 27);
+            this.btnExc.TabIndex = 24;
+            this.btnExc.Text = "Excluir";
+            this.btnExc.UseVisualStyleBackColor = true;
+            // 
+            // btnConf
+            // 
+            this.btnConf.Location = new System.Drawing.Point(555, 230);
+            this.btnConf.Name = "btnConf";
+            this.btnConf.Size = new System.Drawing.Size(112, 27);
+            this.btnConf.TabIndex = 23;
+            this.btnConf.Text = "Confirmar";
+            this.btnConf.UseVisualStyleBackColor = true;
+            this.btnConf.Click += new System.EventHandler(this.btnConf_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(40, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 15);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Quantidade";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 15);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Id do pedido";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(117, 82);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(196, 20);
+            this.txtId.TabIndex = 20;
+            this.txtId.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 22);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Faça seu pedido";
+            // 
+            // dgvList
+            // 
+            this.dgvList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvList.Location = new System.Drawing.Point(370, 22);
+            this.dgvList.Name = "dgvList";
+            this.dgvList.Size = new System.Drawing.Size(331, 180);
+            this.dgvList.TabIndex = 18;
+            this.dgvList.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvList_CellContentClick);
+            this.dgvList.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvList_CellMouseDoubleClick);
+            // 
+            // txtQuant
+            // 
+            this.txtQuant.Location = new System.Drawing.Point(117, 119);
+            this.txtQuant.Name = "txtQuant";
+            this.txtQuant.Size = new System.Drawing.Size(57, 20);
+            this.txtQuant.TabIndex = 28;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(14, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 15);
+            this.label4.TabIndex = 30;
+            this.label4.Text = "Valor do produto";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 302);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtQuant);
+            this.Controls.Add(this.txtVal);
+            this.Controls.Add(this.btnExc);
+            this.Controls.Add(this.btnConf);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtVal;
+        private System.Windows.Forms.Button btnExc;
+        private System.Windows.Forms.Button btnConf;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvList;
+        private System.Windows.Forms.TextBox txtQuant;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Confeitaria2/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Confeitaria2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CarregarDadosBanco();
+        }
+        private void CarregarDadosBanco()
+        {
+            string conexao = "server=localhost;database=db_confeitaria;;uid=root;pwd=etec";
+            MySqlConnection conexaoMYSQL = new MySqlConnection(conexao);
+            conexaoMYSQL.Open();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter("select * from tb_pedido_item", conexaoMYSQL);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dgvList.DataSource = dt;
+
+        }
+
+        private void dgvList_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void dgvList_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            txtId.Text = dgvList.Rows[e.RowIndex].Cells[0].Value.ToString();
+            txtQuant.Text = dgvList.Rows[e.RowIndex].Cells[2].Value.ToString();
+            txtVal.Text = dgvList.Rows[e.RowIndex].Cells[3].Value.ToString();
+        }
+
+        private void btnConf_Click(object sender, EventArgs e)
+        {
+            string conexao = "server=localhost;database=db_confeitaria;uid=root;pwd=etec";
+            MySqlConnection conexaoMYSQL = new MySqlConnection(conexao);
+            conexaoMYSQL.Open();
+            MySqlCommand comando = new MySqlCommand("update tecnico set nome_tec='" + txtId.Text + "', cargo='" + txtQuant.Text + "' where id_tec=" + txtVal.Text, conexaoMYSQL);
+            comando.ExecuteNonQuery();
+            MessageBox.Show("Dados alterados!!!");
+            txtId.Text = "";
+            txtQuant.Text = "";
+            txtVal.Text = "";
+            CarregarDadosBanco();
+        }
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
